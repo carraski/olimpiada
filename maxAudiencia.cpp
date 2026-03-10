ab71c73422768ac81d49747639661e764fa83e0a
carraski <carrasquerrur@ieselaios.com>
2026-03-10 08:59:20 +0000

    inicio algo complicado pero hay cositas

---
+++ maxAudiencia.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+//racha definida como conjunto de partes consecutivas
+//encontrar la racha consecutiva cuya suma total de audiencia sea máxima.
+int main(
+    int racha;
+
+    int n; 
+//n es el numero de pruebas que hay y, por tanto,
+//el numero de rachas que habrá
+cout <<"¿Cuantas pruebas quieres que haya?";
+cin >> n;
+do{
+        int min = -5;
+        int max = 7;
+        int numAle = rand() % (max - min + 1) + min;
+        int secuencia = 
+       
+    if (secuencia < n){
+        cout << secuencia; 
+    }
+    else {
+        cout <<
+    }
+}
+
+
+
+)
