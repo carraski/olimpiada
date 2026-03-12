fb1862c84ec3911dece371fa8eef9aaca83ac0d7
carraski <carrasquerrur@ieselaios.com>
2026-03-12 08:58:49 +0000

    si

--- maxAudJB.cpp
+++ maxAudJB.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <string>
+include namespace std;
 int main(){
 int n;
 string temp;
-vector <string> rachas;	
+vector<string> rachas;	
 rachas[i]
-vector <int> vl;
-cout<<”dime cuantas rachas quieres que te analice”;
+vector<int> vl;
+cout <<”dime cuantas rachas quieres que te analice”;
 cin >> n;
 for (int i=0 ; i<n ; i++){
-getline (cin, temp); rachas.push_back(temp);
+getline (cin, temp); 
+rachas.push_back(temp);
 }
 }
