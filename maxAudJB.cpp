d3256388766c190173ba369809f87e20a4ac596c
carraski <carrasquerrur@ieselaios.com>
2026-03-12 08:48:38 +0000

    un poco de todo

---
+++ maxAudJB.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+int main(){
+int n;
+string temp;
+vector <string> rachas;	
+rachas[i]
+vector <int> vl;
+cout<<”dime cuantas rachas quieres que te analice”;
+cin >> n;
+for (int i=0 ; i<n ; i++){
+getline (cin, temp); rachas.push_back(temp);
+}
+}
